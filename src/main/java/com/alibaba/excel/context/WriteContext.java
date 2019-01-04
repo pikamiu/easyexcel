fea8c316fe4513bfff40bbf5a16ecffd1dc29732
Draher <wujiaming@sailvan.com>
2019-01-04 20:12:12 +0800

    update

--- src/main/java/com/alibaba/excel/context/WriteContext.java
+++ src/main/java/com/alibaba/excel/context/WriteContext.java
@@ -204,7 +204,7 @@ public class WriteContext {
     }
 
     private void addOneRowOfHeadDataToExcel(Row row, List<String> headByRowNum) {
-        StyleUtil.buildRowSttle(row);
+        StyleUtil.buildRowStyle(row);
         if (headByRowNum != null && headByRowNum.size() > 0) {
             for (int i = 0; i < headByRowNum.size(); i++) {
                 Cell cell = WorkBookUtil.createCell(row, i, getCurrentHeadCellStyle(), headByRowNum.get(i));
--- src/main/java/com/alibaba/excel/util/StyleUtil.java
+++ src/main/java/com/alibaba/excel/util/StyleUtil.java
@@ -11,7 +11,7 @@ import java.util.Map;
  */
 public class StyleUtil {
 
-    public static void buildRowSttle(Row row) {
+    public static void buildRowStyle(Row row) {
         row.setHeight((short) 600);
     }
 
